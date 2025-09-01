2818b698a84edeb380888c35760fa9554bb1558f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-01 14:52:56 +0530

    you are eligible to give vote or not

---
+++ canVote.R
@@ -0,0 +1,7 @@
+age <- 17
+
+if(age > 18){
+    print(paste("You are ", age, "years old so you are eligible to vote."))
+} else {
+    print(paste("You are Age", age, "years old so you are not eligible to vote."))
+}
