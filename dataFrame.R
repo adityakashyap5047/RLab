cbd2b7ed0d119440a43a71cbf9d245c2327e750b
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-01 15:51:25 +0530

    sorting the vector

--- dataFrame.R
+++ dataFrame.R
@@ -1,7 +1,7 @@
 friend <- data.frame(
     friend_id = c(1:5), 
     friend_name = c("Ankitu", "Ankit", "Ankita", "Ankitain", "Ankiti"), 
-    stringsAsFactors=FALSE
+    stringsAsFactors = FALSE
 )
 
 print(friend)
