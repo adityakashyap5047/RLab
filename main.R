be9abcd4c5ed944684b9759b978950a221322c5e
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-09-01 14:46:12 +0530

    Started with R

---
+++ main.R
@@ -0,0 +1,12 @@
+print("Hello, R")
+
+x <- 10
+y <- 5
+sum <- x + y
+
+cat("The sum of", x, "and", y, "is:", sum, "\n")
+
+# Generate a sequence
+seq_numbers <- seq(1, 10, by = 2)
+print("Sequence from 1 to 10 with step 2:")
+print(seq_numbers)
